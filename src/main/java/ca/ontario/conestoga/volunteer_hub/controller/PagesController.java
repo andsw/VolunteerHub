17b274b573e1ac7222ea2ba710f13b5b5eecf3d8
Shaowen He <she7744@conestogac.on.ca>
2024-07-05 20:38:17 -0400

    remove login and add avatar after logging.

--- src/main/java/ca/ontario/conestoga/volunteer_hub/controller/PagesController.java
+++ src/main/java/ca/ontario/conestoga/volunteer_hub/controller/PagesController.java
@@ -31,11 +31,52 @@ public class PagesController {
 
   @GetMapping("/home")
   public String home(HttpServletRequest request) {
-    HttpSession session = request.getSession(false);
-    if (session != null && session.getAttribute("username") != null) {
-      return "index";
-    } else {
-      return "redirect:/login";
-    }
+    return "index";
   }
+
+  @GetMapping("/about")
+  public String about() {
+    return "about";
+  }
+
+  @GetMapping("/activity_detail")
+  public String activityDetail() {
+    return "activity_detail";
+  }
+
+  @GetMapping("/activity_list")
+  public String activityList() {
+    return "activity_list";
+  }
+
+  @GetMapping("/contact")
+  public String contact() {
+    return "contact";
+  }
+
+  @GetMapping("/join_activity")
+  public String joinActivity() {
+    return "join_activity";
+  }
+
+  @GetMapping("/msg_confirmation")
+  public String msgConfirmation() {
+    return "msg_confirmation";
+  }
+
+  @GetMapping("/post_activity")
+  public String postActivity() {
+    return "post_activity";
+  }
+
+  @GetMapping("/review_activity")
+  public String reviewActivity() {
+    return "review_activity";
+  }
+
+  @GetMapping("/review_confirmation")
+  public String reviewConfirmation() {
+    return "review_confirmation";
+  }
+
 }
---
+++ src/main/resources/static/js/basic.js
@@ -0,0 +1,6 @@
+// Function to get cookie by name
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
