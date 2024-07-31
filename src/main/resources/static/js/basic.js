bf653ea4e46bf79e64e32c82481bd9c00d404618
Shaowen He <she7744@conestogac.on.ca>
2024-07-31 17:53:19 -0400

    useless frontend files deleted

--- src/main/resources/static/js/basic.js
+++
@@ -1,6 +0,0 @@
-// Function to get cookie by name
-function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-}
